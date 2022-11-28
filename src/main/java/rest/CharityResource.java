fe3f3c71fd1c9fc6ca1949c4a3ef6923a878a663
AndersMeinicke <andersmeinicke@gmail.com>
2022-11-28 11:58:33 +0100

    Added another endpoint, where you can remove one name from the api and it'll not appear, I will later add the "blacklist" to our database so we can permanently blacklist charities.

--- src/main/java/rest/CharityResource.java
+++ src/main/java/rest/CharityResource.java
@@ -2,6 +2,7 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dtos.CharityDTO;
 import dtos.NonProfitDTO;
 import utils.EMF_Creator;
 import utils.HttpUtils;
@@ -13,6 +14,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Path("charity")
@@ -31,14 +34,37 @@ public class CharityResource {
     @Path("{category}")
     public String getAllFromCategory(@PathParam("category") String category) throws IOException {
         Gson gson = new Gson();
-        String nonprofit = HttpUtils.fetchData("https://partners.every.org/v0.2/search/"+category+"?apiKey=2b719ff3063ef1714c32edbfdd7af870&take=50");
-        NonProfitDTO nonProfitDTO = gson.fromJson(nonprofit,NonProfitDTO.class);
+        String nonprofit = HttpUtils.fetchData("https://partners.every.org/v0.2/search/" + category + "?apiKey=2b719ff3063ef1714c32edbfdd7af870&take=50");
+        NonProfitDTO nonProfitDTO = gson.fromJson(nonprofit, NonProfitDTO.class);
         return gson.toJson(nonProfitDTO);
 
     }
-    /*@GET
+
+  @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("{category}+{}")
-    {}*/
+    @Path("{category}+{name}")
+    public String blacklistCharity(@PathParam("category") String category, @PathParam("name") String name) throws IOException {
+        Gson gson = new Gson();
+        String nonprofit = HttpUtils.fetchData("https://partners.every.org/v0.2/search/" + category + "?apiKey=2b719ff3063ef1714c32edbfdd7af870&take=5");
+        NonProfitDTO nonProfitDTO = gson.fromJson(nonprofit, NonProfitDTO.class);
+        List<String> blacklist = new ArrayList<>();
+        blacklist.add(name);
+        List<CharityDTO> removeThese = new ArrayList<>();
+      for (CharityDTO c: nonProfitDTO.getNonprofits()) {
+          for (int i = 0; i < blacklist.size(); i++) {
+              if (c.getSlug().equals(blacklist.get(i))){
+                  removeThese.add(c);
+              }
+          }
+      }
+      for (int i = 0; i < removeThese.size(); i++) {
+          int finalI = i;
+          nonProfitDTO.getNonprofits().removeIf(c ->(c.equals(removeThese.get(finalI))));
+      }
+
+      return gson.toJson(nonProfitDTO);
+    }
+
+
 
 }
