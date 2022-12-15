39a0e9ae57aebce7bf2435f8c51207acb4983989
Oliverden3 <oliversjensen@hotmail.com>
2022-12-15 13:00:13 +0100

    Pray x3

--- src/main/java/security/UserPrincipal.java
+++ src/main/java/security/UserPrincipal.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class UserPrincipal implements Principal {
-
+//hej
 
   private String username;
   private List<String> roles = new ArrayList<>();
